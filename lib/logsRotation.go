b056fc48dd2e5a0ad1be974da87265de627dca8f
Fabien Bellanger <valentil@gmail.com>
2019-04-24 11:58:56 +0200

    Avancement rotation des logs

--- lib/logsRotation.go
+++ lib/logsRotation.go
@@ -2,6 +2,9 @@ package lib
 
 import (
 	"apiticSellers/server/lib"
+	"archive/zip"
+	"errors"
+	"io"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -17,7 +20,7 @@ type logFile struct {
 
 var logFileName string
 
-const NB_FILES_TO_ARCHIVE = 7
+const NB_FILES_TO_ARCHIVE = 5
 
 // ExecuteLogsRotation launches logs rotation
 //
@@ -26,11 +29,13 @@ const NB_FILES_TO_ARCHIVE = 7
 func ExecuteLogsRotation() {
 	logFileName = Config.Log.Filename
 
-	_, err := os.OpenFile(logFileName, os.O_RDONLY, 0755)
+	logFile, err := os.OpenFile(logFileName, os.O_RDONLY, 0755)
 
 	if err != nil {
 		lib.CheckError(err, -1)
 	}
+
+	defer logFile.Close()
 
 	// Recherche des fichiers de log archivés
 	// --------------------------------------
@@ -40,44 +45,13 @@ func ExecuteLogsRotation() {
 		lib.CheckError(err, -2)
 	}
 
-	// Décalage des fichiers d'archivage
-	// ---------------------------------
-	for index := range logFiles {
-		logFiles[index].suffix++
-		logFiles[index].fullname = logFiles[index].name + "." + strconv.Itoa(logFiles[index].suffix)
-	}
+	// Rotation des fichiers
+	// ---------------------
+	makeLogsRotation(logFiles)
 
-	// Renommage des fichiers d'archivage
-	// ----------------------------------
-	for i := len(logFiles) - 1; i >= 0; i-- {
-		_ = os.Rename(logFiles[i].name+"."+strconv.Itoa(logFiles[i].suffix-1), logFiles[i].fullname)
-	}
-
-	// fmt.Println(logFiles)
-
-	// Archivage des NB_FILES_TO_ARCHIVE derniers fichiers
-	// ---------------------------------------------------
-	// TODO
-	/*var buf bytes.Buffer
-	archiveWriter := gzip.NewWriter(&buf)
-
-	for _, file := range logFiles {
-		archiveWriter.Name = file.name
-		archiveWriter.Comment = file.comment
-		archiveWriter.ModTime = file.modTime
-
-		if _, err := archiveWriter.Write([]byte(file.data)); err != nil {
-			log.Fatal(err)
-		}
-
-		if err := archiveWriter.Close(); err != nil {
-			log.Fatal(err)
-		}
-
-		archiveWriter.Reset(&buf)
-	}*/
-
-	// os.Exit(0)
+	// Archivage des fichiers
+	// ----------------------
+	makeLogsArchiving(logFiles)
 
 	// Déplacement du fichier de log
 	// -----------------------------
@@ -89,13 +63,15 @@ func ExecuteLogsRotation() {
 
 	// Création du nouveau fichier logFileName
 	// ----------------------------------------
-	_, err = os.Create(logFileName)
+	logFile, err = os.Create(logFileName)
 
 	if err != nil {
 		// Le fichier de log n'existe pas
 		// ------------------------------
 		lib.CheckError(err, -4)
 	}
+
+	defer logFile.Close()
 }
 
 // findLogFile returns the list of log files
@@ -133,4 +109,119 @@ func findLogFile() ([]logFile, error) {
 	})
 
 	return logFiles, err
+}
+
+// makeLogsRotation makes log rotation by renamming files
+func makeLogsRotation(logFiles []logFile) {
+	// Décalage des fichiers d'archivage
+	// ---------------------------------
+	for index := range logFiles {
+		logFiles[index].suffix++
+		logFiles[index].fullname = logFiles[index].name + "." + strconv.Itoa(logFiles[index].suffix)
+	}
+
+	// Renommage des fichiers d'archivage
+	// ----------------------------------
+	for i := len(logFiles) - 1; i >= 0; i-- {
+		_ = os.Rename(logFiles[i].name+"."+strconv.Itoa(logFiles[i].suffix-1), logFiles[i].fullname)
+	}
+}
+
+// makeLogsArchiving makes logs archiving
+func makeLogsArchiving(logFiles []logFile) error {
+	if len(logFiles) < NB_FILES_TO_ARCHIVE {
+		return errors.New("not enough files to archive")
+	}
+
+	// 0. Recherche du nom de la prochaine archive
+	// -------------------------------------------
+	// TODO
+
+	// 1. Création de l'archive
+	// ------------------------
+	newZipFile, err := os.Create("gin.log.1.zip")
+
+	if err != nil {
+		return err
+	}
+
+	defer newZipFile.Close()
+
+	zipWriter := zip.NewWriter(newZipFile)
+	defer zipWriter.Close()
+
+	// 2. Ajout des fichiers de log à l'archive
+	// ----------------------------------------
+
+	// 2.1. Conversion en tableau de nom de fichier
+	// --------------------------------------------
+	var logFilesName = make([]string, 0)
+
+	for _, file := range logFiles {
+		logFilesName = append(logFilesName, file.fullname)
+	}
+
+	// 2.2. Ajout à l'archive
+	// ----------------------
+	for _, file := range logFilesName {
+		if err = addFileToZip(zipWriter, file); err != nil {
+			return err
+		}
+	}
+
+	// 3. Supression des fichiers logs archivés
+	// ----------------------------------------
+	for _, file := range logFilesName {
+		err = os.Remove(file)
+
+		if err != nil {
+			lib.CheckError(err, 0)
+		}
+	}
+
+	return nil
+}
+
+// addFileToZip adds log file in file archive
+func addFileToZip(zipWriter *zip.Writer, filename string) error {
+	fileToZip, err := os.Open(filename)
+
+	if err != nil {
+		return err
+	}
+
+	defer fileToZip.Close()
+
+	// Get the file information
+	// ------------------------
+	info, err := fileToZip.Stat()
+
+	if err != nil {
+		return err
+	}
+
+	// Get the file header
+	// -------------------
+	header, err := zip.FileInfoHeader(info)
+
+	if err != nil {
+		return err
+	}
+
+	// Using FileInfoHeader() above only uses the basename of the file. If we want
+	// to preserve the folder structure we can overwrite this with the full path.
+	header.Name = filename
+
+	// Change to deflate to gain better compression
+	header.Method = zip.Deflate
+
+	writer, err := zipWriter.CreateHeader(header)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(writer, fileToZip)
+
+	return err
 }
