ad6eb66f72e451d8af946c6943a573f3238f68d6
Fabien Bellanger <valentil@gmail.com>
2019-06-11 12:33:08 +0200

    Modification migration

--- orm/migrations/20191006175832_AddRolesTable.go
+++ orm/migrations/20191006175832_AddRolesTable.go
@@ -9,7 +9,7 @@ import (
 func Migration20191006175832AddRolesTable(db *gorm.DB) {
 	db.AutoMigrate(&models.Role{})
 
-	db.Model(&models.Role{}).AddForeignKey("parent_id", "roles(id)", "CASCADE", "RESTRICT")
+	db.Model(&models.Role{}).AddForeignKey("parent_id", "roles(id)", "CASCADE", "CASCADE")
 
 	db.Model(&models.Role{}).AddIndex("idx_created_at", "created_at")
 	db.Model(&models.Role{}).AddIndex("idx_deleted_at", "deleted_at")
