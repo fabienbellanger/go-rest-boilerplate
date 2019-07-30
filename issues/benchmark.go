466683e66fc8395f140bf72facfdc0ce40addd09
Fabien Bellanger <valentil@gmail.com>
2019-07-30 18:16:00 +0200

    Test benchmark

---
+++ issues/benchmark.go
@@ -0,0 +1,149 @@
+package issues
+
+import (
+	"strconv"
+	"time"
+)
+
+type applicationType struct {
+	ID        int
+	Name      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type moduleType struct {
+	ID            int
+	ApplicationID int
+	Name          string
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
+
+type actionType struct {
+	ID        int
+	ModuleID  int
+	Name      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type sqlDataType struct {
+	ApplicationID        int
+	ApplicationName      string
+	ApplicationCreatedAt time.Time
+	ApplicationUpdatedAt time.Time
+	ModuleID             int
+	ModuleName           string
+	ModuleCreatedAt      time.Time
+	ModuleUpdatedAt      time.Time
+	ActionID             int
+	ActionName           string
+	ActionCreatedAt      time.Time
+	ActionUpdatedAt      time.Time
+}
+
+type DataApplicationType struct {
+	Name      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Modules   map[int]dataModuleType
+}
+
+type dataModuleType struct {
+	Name      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Actions   map[int]dataActionType
+}
+
+type dataActionType struct {
+	Name      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// InitData inits data for test
+func InitData() map[int]DataApplicationType {
+	data := make(map[int]DataApplicationType)
+
+	const nbApplications = 10
+	const nbModules = 1000
+	const nbActions = 100000
+
+	sqlData := make([]sqlDataType, 0)
+	for i := 0; i < nbActions; i++ {
+		line := sqlDataType{
+			ApplicationID:        (i % nbApplications) + 1,
+			ApplicationName:      "Application " + strconv.Itoa((i%nbApplications)+1),
+			ApplicationCreatedAt: time.Now(),
+			ApplicationUpdatedAt: time.Now(),
+			ModuleID:             (i % nbModules) + 1,
+			ModuleName:           "Module " + strconv.Itoa((i%nbModules)+1),
+			ModuleCreatedAt:      time.Now(),
+			ModuleUpdatedAt:      time.Now(),
+			ActionID:             i + 1,
+			ActionName:           "Action " + strconv.Itoa(i+1),
+			ActionCreatedAt:      time.Now(),
+			ActionUpdatedAt:      time.Now(),
+		}
+
+		sqlData = append(sqlData, line)
+	}
+
+	nbData := len(sqlData)
+	for i := 0; i < nbData; i++ {
+		if _, ok := data[sqlData[i].ApplicationID]; !ok {
+			dac := new(dataActionType)
+			dac.Name = sqlData[i].ActionName
+			dac.CreatedAt = sqlData[i].ActionCreatedAt
+			dac.UpdatedAt = sqlData[i].ActionUpdatedAt
+
+			dmo := new(dataModuleType)
+			dmo.Name = sqlData[i].ModuleName
+			dmo.CreatedAt = sqlData[i].ModuleCreatedAt
+			dmo.UpdatedAt = sqlData[i].ModuleUpdatedAt
+			dmo.Actions = make(map[int]dataActionType)
+			dmo.Actions[sqlData[i].ActionID] = *dac
+
+			dap := new(DataApplicationType)
+			dap.Name = sqlData[i].ApplicationName
+			dap.CreatedAt = sqlData[i].ApplicationCreatedAt
+			dap.UpdatedAt = sqlData[i].ApplicationUpdatedAt
+			dap.Modules = make(map[int]dataModuleType)
+			dap.Modules[sqlData[i].ModuleID] = *dmo
+
+			data[sqlData[i].ApplicationID] = *dap
+		}
+
+		if _, ok := data[sqlData[i].ApplicationID].Modules[sqlData[i].ModuleID]; !ok {
+			dac := new(dataActionType)
+			dac.Name = sqlData[i].ActionName
+			dac.CreatedAt = sqlData[i].ActionCreatedAt
+			dac.UpdatedAt = sqlData[i].ActionUpdatedAt
+
+			dmo := new(dataModuleType)
+			dmo.Name = sqlData[i].ModuleName
+			dmo.CreatedAt = sqlData[i].ModuleCreatedAt
+			dmo.UpdatedAt = sqlData[i].ModuleUpdatedAt
+			dmo.Actions = make(map[int]dataActionType)
+			dmo.Actions[sqlData[i].ActionID] = *dac
+
+			data[sqlData[i].ApplicationID].Modules[sqlData[i].ModuleID] = *dmo
+		}
+
+		if _, ok := data[sqlData[i].ApplicationID].Modules[sqlData[i].ModuleID].Actions[sqlData[i].ActionID]; !ok {
+			dac := new(dataActionType)
+			dac.Name = sqlData[i].ActionName
+			dac.CreatedAt = sqlData[i].ActionCreatedAt
+			dac.UpdatedAt = sqlData[i].ActionUpdatedAt
+
+			data[sqlData[i].ApplicationID].Modules[sqlData[i].ModuleID].Actions[sqlData[i].ActionID] = *dac
+		}
+	}
+
+	// jsonData, _ := json.Marshal(data)
+	// fmt.Printf("%v\n", string(jsonData))
+
+	return data
+}
--- main.go
+++ main.go
@@ -1,65 +1,19 @@
 package main
 
-import "github.com/fabienbellanger/go-rest-boilerplate/commands"
+import (
+	"time"
+
+	"github.com/fabienbellanger/go-rest-boilerplate/issues"
+)
 
 func main() {
 	// Lancement de Cobra
-	commands.Execute()
+	// commands.Execute()
+	go issues.InitData()
+	go issues.InitData()
+	go issues.InitData()
+	go issues.InitData()
+	go issues.InitData()
+
+	time.Sleep(10 * time.Second)
 }
-
-// User type
-// type User struct {
-// 	ID        uint64
-// 	Username  string
-// 	Password  string
-// 	Lastname  string
-// 	Firstname string
-// 	CreatedAt time.Time
-// 	UpdatedAt time.Time
-// 	DeletedAt time.Time
-// }
-
-// func main() {
-// 	users := benchmarkEcho()
-// 	// fmt.Println(users)
-
-// 	time.Sleep(20 * time.Second)
-
-// 	s := make([]User, 0, 0)
-// 	copy(s, users)
-// 	users = nil
-
-// 	time.Sleep(30 * time.Second)
-// }
-
-// func benchmarkEcho() []User {
-// 	var user User
-// 	var i uint64
-// 	var n uint64 = 100000
-
-// 	users := make([]User, n, n)
-
-// 	for i = 0; i < n; i++ {
-// 		user.ID = i + 1
-// 		user.Username = "Username jjhfjhfjjfhjf jdhjfhdjhfjd"
-// 		user.Lastname = "Lastname jkfdjkfjkdjkfdkfkd"
-// 		user.Firstname = "Firstname jjhfjhfjjfhjf jdhjfhdjhfjd"
-// 		user.Password = "SFSFDSdddfjdskjfkjdk345345GCTHG5ER9?TG9°?4TN34°AT8°34NTCNRT92°C5T2NV5°E9RNTG°5E9NCG9°ENR9NZ°ERNG9REZNG°ERZNG°RNGR"
-// 		user.CreatedAt = time.Now()
-// 		user.UpdatedAt = time.Now()
-// 		user.DeletedAt = time.Now()
-
-// 		users[i] = user
-// 	}
-// 	// fmt.Println("Before first copy")
-// 	// time.Sleep(20 * time.Second)
-// 	// usersLight := make([]User, 20000, 20000)
-// 	// copy(usersLight, users)
-// 	// users = nil
-
-// 	// time.Sleep(20 * time.Second)
-// 	// fmt.Println("After first copy")
-
-// 	// return usersLight
-// 	return users
-// }
