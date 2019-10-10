a7982d2587a2978c0a9b160f8bdc34ffd8e4633f
Fabien Bellanger <valentil@gmail.com>
2019-10-10 16:42:53 +0200

    Lint

--- database/orm.go
+++ database/orm.go
@@ -3,9 +3,9 @@ package database
 import (
 	"time"
 
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql" // MySQL driver
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/mysql" // MySQL dialect
 	"github.com/spf13/viper"
 
 	"github.com/fabienbellanger/go-rest-boilerplate/lib"
