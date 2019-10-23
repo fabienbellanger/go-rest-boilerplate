26795c4f678ce9a2392a27c5c1d8d21b98f3431b
Fabien Bellanger <valentil@gmail.com>
2019-10-23 17:55:32 +0200

    Correction test unitaire

--- repositories/user/user_mysql_test.go
+++ repositories/user/user_mysql_test.go
@@ -10,10 +10,11 @@ import (
 func TestCheckLogin(t *testing.T) {
 	var username, password string
 	var user, userWanted models.User
+	userHandler := NewMysqlUserRepository()
 
 	username = ""
 	password = ""
-	user, _ = CheckLogin(username, password)
+	user, _ = userHandler.CheckLogin(username, password)
 	if user != userWanted {
 		t.Errorf("CheckLogin(%s, %s) - got: %v, want: %v", username, password, user, userWanted)
 	}
