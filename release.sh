4ee500516b6eecab1113162fb7e2a59cb503e7e3
Fabien Bellanger <valentil@gmail.com>
2019-10-28 15:12:48 +0100

    Amélioration et correction release.sh

--- release.sh
+++ release.sh
@@ -18,7 +18,7 @@ echo
 # 1. Création du répertoire
 # -------------------------
 echo "1. Création du répertoire $VERSION..."
-mkdir -p releases/$VERSION
+mkdir -p "releases/$VERSION"
 echo "   => Répertoire releases/$VERSION créé avec succès"
 RELEASE_DIR=releases/$VERSION
 
@@ -26,11 +26,11 @@ RELEASE_DIR=releases/$VERSION
 # -------------------
 RELEASE_NAME=$APP_NAME"_"$VERSION
 echo "2. Build..."
-go build -o $RELEASE_NAME
-if [ $? -ne 0 ]; then
+
+if ! go build -o "$RELEASE_NAME"; then
   echo "[Erreur] Echec du build"
 
-  rm -rf $RELEASE_DIR
+  rm -rf "$RELEASE_DIR"
   exit 2
 fi
 echo "   => Build créée avec succès"
@@ -38,12 +38,12 @@ echo "   => Build créée avec succès"
 # 3. Déplacement de la release et di fichier de configuration
 # -----------------------------------------------------------
 echo "3. Déplacement des fichiers..."
-mv $RELEASE_NAME $RELEASE_DIR
-cp config.toml $RELEASE_DIR
+mv "$RELEASE_NAME" "$RELEASE_DIR"
+cp config.toml "$RELEASE_DIR"
 echo "   => Déplacement des fichiers réalisé avec succès"
 
 # 4. Renommage du numéro de version dans le fichier de configuration
 # ------------------------------------------------------------------
 echo "4. Renommage du numéro de version dans le fichier de configuration..."
 # sed -i 's//g' RELEASE_DIR/config.toml
-sed -i -e 's/^\(version\s*=\s*\).*$/\1$VERSION/' $RELEASE_DIR/config.toml
+sed -i -e "s/^\(version\s*=\s*\).*$/\1\"$VERSION\"/" "$RELEASE_DIR/config.toml"
